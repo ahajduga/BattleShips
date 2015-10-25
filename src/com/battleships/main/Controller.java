39e9475741422b649dc40b586ce848d21b8a3d78
visst <visthearthstone@wp.pl>
2015-10-25 15:02:57 +0100

    v1.0 select

--- src/com/battleships/main/Controller.java
+++ src/com/battleships/main/Controller.java
@@ -1,8 +1,12 @@
 package com.battleships.main;
 
+import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -17,11 +21,41 @@ public class Controller implements Initializable {
     @FXML
     private GridPane boardRight;
 
+    private Label currentLabel;
+    @FXML
+    private Label mast1Field;
+    @FXML
+    private Label mast2Field;
+    @FXML
+    private Label mast3Field;
+    @FXML
+    private Label mast4Field;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        Circle rect = new Circle(10);
 
-        System.out.println(boardLeft);
-        System.out.println(boardLeft);
-        System.out.println(boardRight);
+        Circle rect1 = new Circle(10);
+        rect1.setFill(Color.BLUE);
+        rect.setFill(Color.RED);
+        boardLeft.add(rect, 0, 0);
+
+        boardLeft.add(rect1, 0, 1);
+
+
+        mast1Field.setText("1m, Left: 4");
+        mast2Field.setText("2m, Left: 3");
+        mast3Field.setText("3m, Left: 2");
+        mast4Field.setText("4m, Left: 1");
+
+    }
+
+    public void shipSelected(Event event) {
+        if (currentLabel != null)
+            currentLabel.setStyle("-fx-background-color:white;");
+        currentLabel = (Label) event.getSource();
+
+        currentLabel.setStyle("-fx-background-color:yellow;");
+
     }
 }
--- src/com/battleships/main/Main.java
+++ src/com/battleships/main/Main.java
@@ -7,12 +7,13 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-
+    //TODO zamienic placeholdery na zmienne w labelach
+    //TODO wstawianie statkow
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setResizable(false);
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Battleships");
         primaryStage.setScene(new Scene(root, 780, 580));
         primaryStage.show();
     }
